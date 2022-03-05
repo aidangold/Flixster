f77f55e6e3a5b8f71c91577727b7f7108c12facc
aidangold <aidanpaulgoldstein@gmail.com>
2022-03-04 21:19:57 -0500

    Now will have embedded youtube function

--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.nfc.Tag
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,9 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 private const val TAG = "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: MutableList<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
@@ -30,10 +33,14 @@ class MovieAdapter(private val context: Context, private val movies: MutableList
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         private val ivposter = itemView.findViewById<ImageView>(R.id.ivposter)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
 
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
@@ -41,6 +48,16 @@ class MovieAdapter(private val context: Context, private val movies: MutableList
             Glide.with(context).load(movie.posterImageUrl).into(ivposter)
 
 
+        }
+
+        override fun onClick(p0: View?) {
+            // get notified on movies that we clicked
+            val movie = movies[adapterPosition]
+            //  TEST Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+            // use intent system to navigate to the new screen
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
     }
 
